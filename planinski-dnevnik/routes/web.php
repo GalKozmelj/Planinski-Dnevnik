546000a41f4ffa07f0d5430ec536af0f3d79797c
Gal <galnik.kozmelj@gmail.com>
2019-02-05 21:01:57 +0100

    login register fixed

--- planinski-dnevnik/routes/web.php
+++ planinski-dnevnik/routes/web.php
@@ -15,6 +15,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
