6c15d6fd848d635c0cb5a5ad5ee5f3be76cd5c22
Gregor-P <george.pirc@gmail.com>
2019-02-25 12:03:32 +0100

    mapa skor dela

--- planinski-dnevnik/resources/views/home.blade.php
+++ planinski-dnevnik/resources/views/home.blade.php
@@ -33,66 +33,21 @@
                     @endif
 
                     You are now online!
-                    {{-- <div style="width: 100%"><iframe width="100%" height="300" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/map-my-route/">Create route map</a></iframe></div><br /> --}}
 
-                    <style>
-                        /* Always set the map height explicitly to define the size of the div
-                         * element that contains the map. */
-                        #map {
-                          height: 100%;
-                        }
-                        /* Optional: Makes the sample page fill the window. */
-
-                      </style>
-
-                    <div id="map"></div>
+                    
                     <script>
-                      // Note: This example requires that you consent to location sharing when
-                      // prompted by your browser. If you see the error "The Geolocation service
-                      // failed.", it means you probably did not give permission for the browser to
-                      // locate you.
-                      var map, infoWindow;
-                      function initMap() {
-                        map = new google.maps.Map(document.getElementById('map'), {
-                          center: {lat: -34.397, lng: 150.644},
-                          zoom: 6
-                        });
-                        infoWindow = new google.maps.InfoWindow;
-                
-                        // Try HTML5 geolocation.
-                        if (navigator.geolocation) {
-                          navigator.geolocation.getCurrentPosition(function(position) {
-                            var pos = {
-                              lat: position.coords.latitude,
-                              lng: position.coords.longitude
-                            };
-                
-                            infoWindow.setPosition(pos);
-                            infoWindow.setContent('Location found.');
-                            infoWindow.open(map);
-                            map.setCenter(pos);
-                          }, function() {
-                            handleLocationError(true, infoWindow, map.getCenter());
-                          });
-                        } else {
-                          // Browser doesn't support Geolocation
-                          handleLocationError(false, infoWindow, map.getCenter());
-                        }
-                      }
-                
-                      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-                        infoWindow.setPosition(pos);
-                        infoWindow.setContent(browserHasGeolocation ?
-                                              'Error: The Geolocation service failed.' :
-                                              'Error: Your browser doesn\'t support geolocation.');
-                        infoWindow.open(map);
-                      }
+                    navigator.geolocation.getCurrentPosition(function(position){
+                        var lati =position.coords.latitude;
+                        var long = position.coords.longitude;
+                        var velenje = {lat: lati, lng: long};
+                    });
                     </script>
-                    <script async defer
-                    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDKS99WQSkH26BMXvdB8FxHds2yeg93OjY&callback=initMap">
-                    </script>
-
-
+                            
+                    <div style="width: 100%">
+                        <iframe src="https://www.google.com/maps/embed?
+                        pb=!1m18!1m12!1m3!1d2314.9443297912608!2d15.121500213349647!3d46.37041437882164!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4765612f225497d5%3A0x6e9c117edab89adb!2sMe%C5%A1kova+ulica+3%2C+3320+Velenje!5e0!3m2!1ssl!2ssi!4v1551091782448" width="600" height="450" frameborder="0" style="border:0" allowfullscreen>
+                        </iframe>
+                    </div>
                 </div>
             </div>
         </div>
