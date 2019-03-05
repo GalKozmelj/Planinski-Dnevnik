af948d4debf5a401eda4028b9ac8f099491b8eaf
Gal <galnik.kozmelj@gmail.com>
2019-03-05 10:02:09 +0100

    LEAFLET FIND LOCATION

--- planinski-dnevnik/resources/views/home.blade.php
+++ planinski-dnevnik/resources/views/home.blade.php
@@ -38,14 +38,23 @@
                     <div style="height: 180px;" id="mapid"></div>
 
                     <script>
-                        var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-                        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZ2Fsa296bWVsaiIsImEiOiJjanN2ajRqNnowM3lyNDNxazY0cXFubXFnIn0.asRIOOwyMjgXxKGLYcZG-w', {
-                        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                        
+                    navigator.geolocation.getCurrentPosition(function(location) {
+                    var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
+
+                    var mymap = L.map('mapid').setView(latlng, 13)
+                    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+                        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://mapbox.com">Mapbox</a>',
                         maxZoom: 18,
                         id: 'mapbox.streets',
-                        accessToken: 'your.mapbox.access.token'
+                        accessToken: 'pk.eyJ1IjoiYmJyb29rMTU0IiwiYSI6ImNpcXN3dnJrdDAwMGNmd250bjhvZXpnbWsifQ.Nf9Zkfchos577IanoKMoYQ'
                     }).addTo(mymap);
+
+                    var marker = L.marker(latlng).addTo(mymap);
+                    });
+
+
                     </script>
 
 
