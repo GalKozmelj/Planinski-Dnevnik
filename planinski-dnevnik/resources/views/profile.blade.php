ea78f8dc1c6e7c30b792d0247cc29cbb85041b34
Gal <galnik.kozmelj@gmail.com>
2019-03-26 09:56:50 +0100

    update profile

--- planinski-dnevnik/resources/views/profile.blade.php
+++ planinski-dnevnik/resources/views/profile.blade.php
@@ -1,10 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-  Izbrana lokacija:
-  
-  {{$location->height}}
+<div class="container">
+
+ 
+<div class="row justify-content-center">
+    <div class="col-md-8">
+
+    <div class="card" style="color: #93afbb;margin-bottom:10px;">
+            <h1 style="text-align:center;padding:10px;">{{$location->name}}</h1>
+            <hr>
+                <table cellpadding="10">
+                <tr><td>Nadmorska Visina:</td><td>{{$location->height}}</td></tr>
+                <tr><td>Opis:</td><td>{{$location->description}}</td></tr>
+                <tr><td>latitude:</td><td>{{$location->lat}}</td></tr>
+                <tr><td>longtitude:</td><td>{{$location->lon}}</td></tr>
+                </table>
+    </div>
+
+    <div class="card" style="color: #93afbb">
+            <h1 style="text-align:center;padding:10px;">Objave</h1>
+            <hr>
+            
+    </div>
 
 
 
+    </div>
+</div>
 @endsection
