a9944920c90501aaef0ead8dd1fd45907aa0f8cd
Gregor-P <george.pirc@gmail.com>
2019-03-26 09:03:41 +0100

    asdasdasd

--- planinski-dnevnik/resources/views/home.blade.php
+++ planinski-dnevnik/resources/views/home.blade.php
@@ -7,12 +7,12 @@
  
     <div class="row justify-content-center">
         <div class="col-md-8">
-            
 
 
 
-     
 
+
+    
             <div class="card">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
