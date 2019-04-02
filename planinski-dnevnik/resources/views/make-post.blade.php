849ac4d32bc456e6d2eec2717bf7a265a402d44e
Gal <galnik.kozmelj@gmail.com>
2019-04-02 08:29:42 +0200

    text-align

--- planinski-dnevnik/resources/views/make-post.blade.php
+++ planinski-dnevnik/resources/views/make-post.blade.php
@@ -1,5 +1,5 @@
 
-<div class="card" style="margin-top: 5px; color: black;">
+<div class="card" style="margin-top: 5px; color: black; text-align:center">
 Trenutno ste na/v:  <a href="/profile/{{$loc->name}}">{{$loc->name}}</a>
 
   {{ Form::open(array('route' => 'posts.store')) }}
@@ -8,6 +8,7 @@ Trenutno ste na/v:  <a href="/profile/{{$loc->name}}">{{$loc->name}}</a>
     {{ Form::hidden('user_id', Auth::user()->id, ['required' => 'required'])}}
     {{ Form::hidden('lat', $lat, ['required' => 'required'])}}
     {{ Form::hidden('lon', $lon, ['required' => 'required'])}}
+    <br>
     {{ Form::submit('Dodaj') }}
   {{Form::close()}}
 </div>
