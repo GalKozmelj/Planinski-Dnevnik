42f7f97e124c331c4f711bae6bf53af47820b6ce
Gal <galnik.kozmelj@gmail.com>
2019-04-02 08:40:58 +0200

    style for make post

--- planinski-dnevnik/resources/views/make-post.blade.php
+++ planinski-dnevnik/resources/views/make-post.blade.php
@@ -1,18 +1,18 @@
 
-<div class="card" style="margin-top: 5px; color: black;">
+<div class="card" style="margin-top: 5px; color: black;text-align:center">
   @php
       
   @endphp
 Trenutno ste na/v:  {{link_to_action('LocationController@redirect', 'trenutna lokacija', ['location_name'=>$loc->name])}}
 
   {{ Form::open(array('route' => 'posts.store')) }}
-    {{ Form::textarea('content', '', ['required' => 'required', 'placeholder' => 'Post content'])}}
+    {{ Form::textarea('content', '', ['required' => 'required', 'placeholder' => 'Post content', 'style' => 'width:100%; height:200px; border: 1px solid #abe'])}}
     {{ Form::hidden('location_id', $loc->id, ['required' => 'required'])}}
     {{ Form::hidden('user_id', Auth::user()->id, ['required' => 'required'])}}
     {{ Form::hidden('lat', $lat, ['required' => 'required'])}}
     {{ Form::hidden('lon', $lon, ['required' => 'required'])}}
     <br>
-    {{ Form::submit('Dodaj') }}
+    {{ Form::submit('Dodaj', ['style' => 'width:100%;background-color:#abe; text-align:center;padding:10px;color:white; border: 0px']) }}
   {{Form::close()}}
 </div>
 
